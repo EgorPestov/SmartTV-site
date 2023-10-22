a8e6f518c21865dbe328708f25ac5d08df02e7d5
EgorPestov <egorpestov2012@yandex.ru>
2023-10-22 16:52:01 +0400

    feat: adds OK button

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -3,5 +3,6 @@ import { BannerImage } from "./banner-image"
 export const Banner = () =>
     <div className="banner" >
         <BannerImage />
+        <button className="ok-button">ОК</button>
     </div>
   
