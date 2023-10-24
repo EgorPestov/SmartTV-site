975f97792684df2113dcfc1f78a965b02e0e9b28
EgorPestov <egorpestov2012@yandex.ru>
2023-10-24 14:57:07 +0400

    feat: adds movement between display keyboard keys WIP

--- src/components/input-form/input-form.tsx
+++ src/components/input-form/input-form.tsx
@@ -1,19 +1,25 @@
 import { CloseButton } from "../close-button/close-button";
 import { useAppDispatch } from "../../hooks/use-app-dispatch/use-app-dispatch";
 import { setFormStatus, setVideoStatus } from "../../store/banner-process/banner-process";
-import { useState, ChangeEvent, MouseEvent, useRef } from 'react';
+import { useState, ChangeEvent, MouseEvent, useRef, useEffect } from 'react';
 import InputMask from 'react-input-mask';
-
+import { useKeyPress } from "../../assets/hooks/use-key-press/use-key-press";
 
 export const InputForm = () => {
     const dispatch = useAppDispatch();
     const inputRef = useRef<HTMLInputElement | null>(null);
     const [numberString, setNumberString] = useState('+7(___)___-__-__');
+    const [focusedIndex, setFocusedIndex] = useState(0);
+
+    const arrowUpPressed = useKeyPress('ArrowUp');
+    const arrowDownPressed = useKeyPress('ArrowDown');
+    const arrowLeftPressed = useKeyPress('ArrowLeft');
+    const arrowRightPressed = useKeyPress('ArrowRight');
 
     const handleNumberChange = (evt: ChangeEvent<HTMLInputElement>) => {
         setNumberString(evt.target.value);
         if (evt.target.value === '') {
-            setNumberString('+7(___)___-__-__')
+            setNumberString('+7(___)___-__-__');
         }
     };
 
@@ -63,7 +69,7 @@ export const InputForm = () => {
 
     const handleDeleteClick = (evt: MouseEvent<HTMLButtonElement>) => {
         evt.preventDefault();
-        if (numberString === '+7(___)___-__-__'){
+        if (numberString === '+7(___)___-__-__') {
             return;
         }
         const lastDigitIndex = numberString.search(/\d(?![\d\D]*\d)/);
@@ -76,6 +82,51 @@ export const InputForm = () => {
         }, 0);
     };
 
+    const handleInputKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Предотвращаем отправку формы по умолчанию
+        }
+    };
+
+    useEffect(() => {
+        const maxIndex = 12;
+        if (arrowUpPressed) {
+            if (focusedIndex === 2) {
+                setFocusedIndex(10);
+            } else if (focusedIndex === 3) {
+                setFocusedIndex(11);
+            } else if (focusedIndex === 11) {
+                setFocusedIndex(9);
+            } else (setFocusedIndex((prevIndex) => (prevIndex - 3 + maxIndex) % maxIndex));
+        }
+        if (arrowDownPressed) {
+            if (focusedIndex === 8) {
+                setFocusedIndex(10);
+            } else if (focusedIndex === 9) {
+                setFocusedIndex(11);
+            } else if (focusedIndex === 11) {
+                setFocusedIndex(3);
+            } else (setFocusedIndex((prevIndex) => (prevIndex + 3) % maxIndex));
+                }
+        if (arrowLeftPressed) {
+            if (focusedIndex === 1) {
+                setFocusedIndex(12);
+            }
+            setFocusedIndex((prevIndex) => (prevIndex - 1 + maxIndex) % maxIndex);
+        }
+        if (arrowRightPressed) {
+            if (focusedIndex === 11) {
+                setFocusedIndex(0);
+            }
+            setFocusedIndex((prevIndex) => (prevIndex + 1) % maxIndex);
+        }
+
+        const focusedElement = document.querySelector('.focused'); // Находим элемент с классом "focused"
+        if (focusedElement) {
+            focusedElement.focus(); // Устанавливаем фокус на найденном элементе
+        }
+    }, [arrowUpPressed, arrowDownPressed, arrowLeftPressed, arrowRightPressed]);
+
     return (
         <>
             <form className="input-form-wrapper" onClick={handleOutOfMaskClick}>
@@ -86,6 +137,7 @@ export const InputForm = () => {
                         maskChar="_"
                         value={numberString}
                         onChange={handleNumberChange}
+                        onKeyDown={handleInputKeyDown}
                     >
                         {() => <input
                             type="text"
@@ -94,26 +146,26 @@ export const InputForm = () => {
                             onClick={() => moveCursorToEnd()}
                         />}
                     </InputMask>
-                    <p className='input-form-top-text'>и с Вами свяжется наш менеждер для дальнейшей консультации</p>
+                    <p className='input-form-top-text'>и с Вами свяжется наш менеджер для дальнейшей консультации</p>
                     <div className="buttons-container">
                         <div className="buttons-row">
-                            <button className="button-usual" onClick={(e) => handleNumberClick('1', e)}>1</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('2', e)}>2</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('3', e)}>3</button>
+                            <button className={`button-usual ${focusedIndex === 1 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('1', e)}>1</button>
+                            <button className={`button-usual ${focusedIndex === 2 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('2', e)}>2</button>
+                            <button className={`button-usual ${focusedIndex === 3 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('3', e)}>3</button>
                         </div>
                         <div className="buttons-row">
-                            <button className="button-usual" onClick={(e) => handleNumberClick('4', e)}>4</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('5', e)}>5</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('6', e)}>6</button>
+                            <button className={`button-usual ${focusedIndex === 4 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('4', e)}>4</button>
+                            <button className={`button-usual ${focusedIndex === 5 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('5', e)}>5</button>
+                            <button className={`button-usual ${focusedIndex === 6 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('6', e)}>6</button>
                         </div>
                         <div className="buttons-row">
-                            <button className="button-usual" onClick={(e) => handleNumberClick('7', e)}>7</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('8', e)}>8</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('9', e)}>9</button>
+                            <button className={`button-usual ${focusedIndex === 7 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('7', e)}>7</button>
+                            <button className={`button-usual ${focusedIndex === 8 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('8', e)}>8</button>
+                            <button className={`button-usual ${focusedIndex === 9 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('9', e)}>9</button>
                         </div>
                         <div className="buttons-row">
-                            <button className="button-usual button-double" onClick={handleDeleteClick}>Стереть</button>
-                            <button className="button-usual" onClick={(e) => handleNumberClick('0', e)}>0</button>
+                            <button className={`button-usual button-double ${focusedIndex === 10 ? 'focused' : ''}`} onClick={handleDeleteClick}>Стереть</button>
+                            <button className={`button-usual ${focusedIndex === 11 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('0', e)}>0</button>
                         </div>
                     </div>
                 </fieldset>
