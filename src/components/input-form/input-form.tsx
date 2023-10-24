0819630b44890db0bb205d0a363a1ff8b599ab84
EgorPestov <egorpestov2012@yandex.ru>
2023-10-24 16:43:59 +0400

    fix: small fixes

--- src/components/input-form/input-form.tsx
+++ src/components/input-form/input-form.tsx
@@ -1,7 +1,7 @@
 import { CloseButton } from "../close-button/close-button";
 import { useAppDispatch } from "../../hooks/use-app-dispatch/use-app-dispatch";
 import { setFormStatus, setVideoStatus } from "../../store/banner-process/banner-process";
-import { useState, ChangeEvent, MouseEvent, useRef, useEffect } from 'react';
+import { useState, ChangeEvent, MouseEvent, KeyboardEvent, useRef, useEffect } from 'react';
 import InputMask from 'react-input-mask';
 import { useKeyPress } from "../../assets/hooks/use-key-press/use-key-press";
 
@@ -15,6 +15,8 @@ export const InputForm = () => {
     const arrowDownPressed = useKeyPress('ArrowDown');
     const arrowLeftPressed = useKeyPress('ArrowLeft');
     const arrowRightPressed = useKeyPress('ArrowRight');
+
+
 
     const handleNumberChange = (evt: ChangeEvent<HTMLInputElement>) => {
         setNumberString(evt.target.value);
@@ -52,6 +54,7 @@ export const InputForm = () => {
 
     const handleNumberClick = (digit: string, evt: MouseEvent<HTMLButtonElement>) => {
         evt.preventDefault();
+        const currentFocusedElement = document.activeElement;
         if (inputRef.current) {
             inputRef.current.focus();
         }
@@ -64,27 +67,37 @@ export const InputForm = () => {
         }
         setTimeout(() => {
             moveCursorToEnd();
+            if (currentFocusedElement) {
+                (currentFocusedElement as HTMLElement).focus();
+            }
         }, 0);
     };
 
     const handleDeleteClick = (evt: MouseEvent<HTMLButtonElement>) => {
         evt.preventDefault();
-        if (numberString === '+7(___)___-__-__') {
-            return;
-        }
+        const currentFocusedElement = document.activeElement;
+
         const lastDigitIndex = numberString.search(/\d(?![\d\D]*\d)/);
         if (lastDigitIndex >= 0) {
+            if (lastDigitIndex === 1) {
+                return;
+            }
             const updatedNumberString = numberString.substring(0, lastDigitIndex) + '_' + numberString.substring(lastDigitIndex + 1);
+            
+            console.log(updatedNumberString)
             setNumberString(updatedNumberString);
         }
         setTimeout(() => {
             moveCursorToEnd();
+            if (currentFocusedElement) {
+                (currentFocusedElement as HTMLElement).focus();
+            }
         }, 0);
     };
 
-    const handleInputKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault(); // Предотвращаем отправку формы по умолчанию
+    const handleInputKeyDown = (evt: KeyboardEvent<HTMLInputElement>) => {
+        if (evt.key === 'Enter') {
+            evt.preventDefault();
         }
     };
 
@@ -106,8 +119,10 @@ export const InputForm = () => {
                 setFocusedIndex(11);
             } else if (focusedIndex === 11) {
                 setFocusedIndex(3);
+            } else if (focusedIndex === 0) {
+                setFocusedIndex(1);
             } else (setFocusedIndex((prevIndex) => (prevIndex + 3) % maxIndex));
-                }
+        }
         if (arrowLeftPressed) {
             if (focusedIndex === 1) {
                 setFocusedIndex(12);
@@ -123,7 +138,7 @@ export const InputForm = () => {
 
         const focusedElement = document.querySelector('.focused'); // Находим элемент с классом "focused"
         if (focusedElement) {
-            focusedElement.focus(); // Устанавливаем фокус на найденном элементе
+            (focusedElement as HTMLElement).focus(); // Устанавливаем фокус на найденном элементе
         }
     }, [arrowUpPressed, arrowDownPressed, arrowLeftPressed, arrowRightPressed]);
 
@@ -138,6 +153,7 @@ export const InputForm = () => {
                         value={numberString}
                         onChange={handleNumberChange}
                         onKeyDown={handleInputKeyDown}
+                        alwaysShowMask={true}
                     >
                         {() => <input
                             type="text"
@@ -149,23 +165,23 @@ export const InputForm = () => {
                     <p className='input-form-top-text'>и с Вами свяжется наш менеджер для дальнейшей консультации</p>
                     <div className="buttons-container">
                         <div className="buttons-row">
-                            <button className={`button-usual ${focusedIndex === 1 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('1', e)}>1</button>
-                            <button className={`button-usual ${focusedIndex === 2 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('2', e)}>2</button>
-                            <button className={`button-usual ${focusedIndex === 3 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('3', e)}>3</button>
+                            <button className={`button-usual ${focusedIndex === 1 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('1', e)}>1</button>
+                            <button className={`button-usual ${focusedIndex === 2 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('2', e)}>2</button>
+                            <button className={`button-usual ${focusedIndex === 3 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('3', e)}>3</button>
                         </div>
                         <div className="buttons-row">
-                            <button className={`button-usual ${focusedIndex === 4 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('4', e)}>4</button>
-                            <button className={`button-usual ${focusedIndex === 5 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('5', e)}>5</button>
-                            <button className={`button-usual ${focusedIndex === 6 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('6', e)}>6</button>
+                            <button className={`button-usual ${focusedIndex === 4 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('4', e)}>4</button>
+                            <button className={`button-usual ${focusedIndex === 5 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('5', e)}>5</button>
+                            <button className={`button-usual ${focusedIndex === 6 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('6', e)}>6</button>
                         </div>
                         <div className="buttons-row">
-                            <button className={`button-usual ${focusedIndex === 7 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('7', e)}>7</button>
-                            <button className={`button-usual ${focusedIndex === 8 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('8', e)}>8</button>
-                            <button className={`button-usual ${focusedIndex === 9 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('9', e)}>9</button>
+                            <button className={`button-usual ${focusedIndex === 7 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('7', e)}>7</button>
+                            <button className={`button-usual ${focusedIndex === 8 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('8', e)}>8</button>
+                            <button className={`button-usual ${focusedIndex === 9 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('9', e)}>9</button>
                         </div>
                         <div className="buttons-row">
                             <button className={`button-usual button-double ${focusedIndex === 10 ? 'focused' : ''}`} onClick={handleDeleteClick}>Стереть</button>
-                            <button className={`button-usual ${focusedIndex === 11 ? 'focused' : ''}`} onClick={(e) => handleNumberClick('0', e)}>0</button>
+                            <button className={`button-usual ${focusedIndex === 11 ? 'focused' : ''}`} type="button" onClick={(e) => handleNumberClick('0', e)}>0</button>
                         </div>
                     </div>
                 </fieldset>
