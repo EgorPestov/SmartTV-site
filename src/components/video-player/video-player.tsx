884db3ced7ee4f7820fc3b7df40eaab6c515d944
EgorPestov <egorpestov2012@yandex.ru>
2023-10-25 17:06:19 +0400

    feat: adds pausing of video

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -3,9 +3,10 @@ import { useRef, useState } from 'react';
 import Video from '../../assets/videos/Volvo Trucks - The Epic Split feat. Van Damme (Live Test).webm';
 import { useAppSelector } from '../../hooks/use-app-selector/use-app-selector';
 import { getVideoShowStatus } from '../../store/banner-process/selectors';
+import { useEffect } from 'react';
 
 export const VideoPlayer = () => {
-    const videoRef = useRef<HTMLVideoElement | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(true);
 
   const handleVideoClick = () => {
@@ -21,6 +22,18 @@ export const VideoPlayer = () => {
 
   const isVideoShowing = useAppSelector(getVideoShowStatus);
 
+  useEffect(() => {
+    if (videoRef.current) {
+      if (isVideoShowing) {
+        videoRef.current.play();
+        setIsPlaying(true);
+      } else {
+        videoRef.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  }, [isVideoShowing]);
+
   return (
     <div className={isVideoShowing ? '' : 'visually-hidden'}>
       <video
@@ -31,9 +44,9 @@ export const VideoPlayer = () => {
         muted
         onClick={handleVideoClick}
       >
-        <source 
-        src={Video} 
-        type="video/webm" 
+        <source
+          src={Video}
+          type="video/webm"
         />
       </video>
     </div>
